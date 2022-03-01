84be30157ca85bd6be04c3569f6981bc4cad6f6f
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-03-01 19:08:43 +0600

    phone data loaded

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,8 @@
-console.log("I'm linked....")
+
+document.getElementById('search-button').addEventListener('click', function () {
+    const searchText = document.getElementById('input-area').value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
