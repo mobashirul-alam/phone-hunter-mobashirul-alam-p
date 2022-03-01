c8de5a5da9e4f65382a361450c6b0d6183053aa8
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-03-01 20:25:39 +0600

    phone data displayed

--- JS/app.js
+++ JS/app.js
@@ -4,5 +4,33 @@ document.getElementById('search-button').addEventListener('click', function () {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayPhone(data.data))
 })
+
+const displayPhone = phones => {
+    const displayResult = document.getElementById('display-results');
+    for (const phone of phones) {
+        // console.log(phone.slug)
+        const div = document.createElement('div');
+        div.classList.add('col')
+        div.innerHTML = `
+            <div class="card h-100 p-3 bg-light">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">Brand: <span class="fw-bold">${phone.brand}</span></p>
+                    <button onclick="phoneDetails('${phone.slug}')" class="btn btn-info text-light">Details</button>
+                </div>
+            </div>
+        `
+        displayResult.appendChild(div)
+    }
+}
+// ---------- Phone details in top ----------
+const phoneDetails = id => {
+    // console.log(id)
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+}
